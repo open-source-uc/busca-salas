b2e3141ff138c1cf1761ce62fa321b5f4571bddf
Benjamín Vicente <benjavicente@uc.cl>
2021-09-17 22:15:55 -0300

    chore: update seed migration script

--- scripts/transform_original_data.py
+++ scripts/transform_original_data.py
@@ -1,104 +1,102 @@
+from __future__ import annotations
+
 import json
-from os import EX_CANTCREAT
 from pathlib import Path
+import datetime
 
-HAS_TOML = True
-try:
-    import toml
-except ImportError:
-    HAS_TOML = False
+from polylabel import polylabel
 
-HAS_YAML = True
-try:
-    import yaml
-except ImportError:
-    HAS_YAML = False
+import toml
+import yaml
 
 
 class UnusableDataException(Exception):
     pass
 
 
-def get_campus(element):
+def dict_drop_empty(**obj):
+    return {k: v for k, v in obj.items() if v not in (None, "", {})}
+
+
+def get_campus(element: dict):
     if "parent" in element and element["parent"] and "campus" in element["parent"]:
         return element["parent"]["campus"]
     if "campus" in element:
         return element["campus"]
 
 
-def get_polygon(location):
+def get_polygon(location: dict) -> None | list[tuple[float, float]]:
     if location["type"] == "Point":
         return None
     coordinates = location["coordinates"]
     while coordinates[0] and not isinstance(coordinates[0][0], float):
         coordinates = coordinates[0]
     if not (coordinates and coordinates[0]):
-        raise UnusableDataException
+        raise UnusableDataException("Bad polygon")
     return coordinates
 
 
-def get_lat_lng(location):
+def get_lat_lng(location: dict):
     if location["type"] == "Polygon":
-        lat_sum, lng_sum = (0, 0)
         polygon = get_polygon(location)
-        if not polygon:
-            raise UnusableDataException
-        for lat, lng in polygon:
-            lat_sum += lat
-            lng_sum += lng
-        count = len(polygon)
-        return [lat_sum / count, lng_sum / count]
-    if location["type"] == "Point":
+        if polygon:
+            return polylabel([polygon])
+    elif location["type"] == "Point":
         return location["coordinates"]
-    raise ValueError
+    raise UnusableDataException("Mising location")
 
 
-def get_parent_id(child, all_elements):
+def get_parent_id(child: dict, all_elements: dict):
     if not child["parent"]:
         return None
     for i, element in enumerate(all_elements):
         if child["parent"]["identifier"] == element["identifier"]:
             return i
-    raise ValueError
+    raise UnusableDataException("Parent not found")
 
 
-def get_contact(element, name):
+def get_contact(element, name) -> None | list:
     if ("contact" in element) and (name in element["contact"]):
         return element["contact"][name]
     return None
 
 
-def get_social(element, name):
+def get_social(element, name) -> None | str:
     social = get_contact(element, "social")
-    if not social:
-        return None
-
-    for social in social:
-        if social["type"] == name:
-            return social["url"]
+    if social:
+        for social in social:
+            if social["type"] == name:
+                return social["url"]
     return None
 
+def get_floor(element):
+    floor: str | None = element.get("floor", None)
+    if floor:
+        floor = floor.strip(" ?")
+        if floor.isnumeric():
+            return int(floor)
+    return None
 
-def get_element_properties(element, i, all_elements):
+def get_element_properties(element: dict, i, all_elements):
     if not "location" in element:
-        raise UnusableDataException
+        raise UnusableDataException("Doesn't have a location")
     phones = get_contact(element, "phones")
     urls = get_contact(element, "urls")
     emails = get_contact(element, "emails")
-    return dict(
+    return dict_drop_empty(
         id=i,
         name=element["name"],
-        location=dict(
-            campus=get_campus(element),
-            latLng=get_lat_lng(element["location"]),
-            floor=element.get("floor", None),
-            parentId=get_parent_id(element, all_elements),
-            polygon=get_polygon(element["location"]),
-        ),
-        contact=dict(
-            url=urls[0] if urls else "",
-            phone=phones[0] if phones else "",
-            email=emails[0] if emails else "",
+        categories=element.get("categories", None),
+        description=element.get("information", None),
+        campus=get_campus(element),
+        latLng=get_lat_lng(element["location"]),
+        floor=get_floor(element["location"]),
+        parentId=get_parent_id(element, all_elements),
+        polygon=get_polygon(element["location"]),
+        contact=dict_drop_empty(
+            url=urls[0] if urls else None,
+            phone=phones[0] if phones else None,
+            email=emails[0] if emails else None,
             instagram=get_social(element, "instagram"),
             twitter=get_social(element, "twitter"),
             facebook=get_social(element, "facebook"),
@@ -106,12 +104,19 @@ def get_element_properties(element, i, all_elements):
     )
 
 
+def dump(module, data: list, **kwargs):
+    now = datetime.datetime.now(None).isoformat(timespec="minutes")
+    with Path("assets", f"data.{module.__name__}").open("w") as file:
+        module.dump({"updated_at": now, "places": data}, file, **kwargs)
+
+
 def main():
+    # Load seeds
     data = []
-    for path in Path("assets", "seeds").iterdir():
-        if path.suffix == ".json":
-            with path.open() as file:
-                data.extend(json.load(file))
+    for path in Path("assets", "seeds").glob("*.json"):
+        with path.open() as file:
+            data.extend(json.load(file))
+    # Change schema
     new_data = []
     for i, element in enumerate(data):
         try:
@@ -119,16 +124,10 @@ def main():
             new_data.append(new_element)
         except UnusableDataException:
             print(element)
-
-    with Path("assets", "data.json").open("w") as file:
-        json.dump(new_data, file, ensure_ascii=False, indent=2)
-
-    if HAS_TOML:
-        with Path("assets", "data.toml").open("w") as file:
-            toml.dump({"place": new_data}, file)
-    if HAS_YAML:
-        with Path("assets", "data.yaml").open("w") as file:
-            yaml.dump({"place": new_data}, file)
+    # Dump
+    dump(json, new_data, ensure_ascii=False, indent=2)
+    dump(toml, new_data)
+    dump(yaml, new_data, allow_unicode=True)
 
 
 if __name__ == "__main__":
