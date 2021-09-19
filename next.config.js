dc3b1a58d28493cf5807d7f0d76b0f017fe1d776
Benjamín Vicente <benjavicente@uc.cl>
2021-09-19 16:59:19 -0300

    fix: exclude social images & add locales

--- next.config.js
+++ next.config.js
@@ -4,11 +4,30 @@ const path = require("path")
 const withPWA = require("next-pwa")
 
 /**
+ * NOTE: next-pwa generates public/sw.js, witch prefetches and routes
+ * every file under the public directory. This increases significantly
+ * the PWA size. The configuration option `pwa.publicExcludes` is not
+ * enough, since some files should be included only in specific scenarios
+ * (`img/apple` for PWA in Apple devices and `img/social` for meta tags).
+ *
+ * This should be addressed in the future.
+ * A temporally workaround would be to optimize static sizes during build.
+ *
+*/
+
+/**
  * @type {import('next').NextConfig}
  */
-module.exports = withPWA({
+const nextConfig = {
+  i18n: {
+    defaultLocale: "es-CL",
+    locales: ["es-CL"]
+  },
   includePaths: [path.join(__dirname, "styles")],
   pwa: {
-    dest: "public"
+    dest: "public",
+    publicExcludes: ["!img/social/**/*"]
   }
-})
+}
+
+module.exports = withPWA(nextConfig)
