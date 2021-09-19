2d7812d3fe74d4a9d03acb31b3acd643b15c29dc
Benjamín Vicente <benjavicente@uc.cl>
2021-09-19 00:38:40 -0300

    fix: types and package license

--- lib/helpers.ts
+++ lib/helpers.ts
@@ -2,9 +2,8 @@ export function unsplash(str: TemplateStringsArray) {
   return `https://source.unsplash.com/${str[0]}/400x300`
 }
 
-type Object<V> = { [key: string]: V }
 type Mapper<V, R> = (key: string, value: V) => R
 
-export function objMap<V, R>(obj: Object<V>, fn: Mapper<V, R>): R[] {
+export function objMap<V, R>(obj: Record<string, V>, fn: Mapper<V, R>): R[] {
   return Object.entries(obj).map(([k, v]) => fn(k, v))
 }
